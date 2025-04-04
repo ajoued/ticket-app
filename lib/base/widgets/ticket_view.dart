b1b545e32ed9c262d56e4519dd64103231e97e34
ajoued <ajoueds@gmail.com>
2025-04-04 15:35:59 +0100

    added the first line of information in the ticket widget

--- lib/base/widgets/ticket_view.dart
+++ lib/base/widgets/ticket_view.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
+import 'package:ticket_app/base/widgets/app_layout_builder_widget.dart';
 import 'package:ticket_app/base/widgets/big_dot.dart';
 
 class TicketView extends StatelessWidget {
@@ -21,24 +24,81 @@ class TicketView extends StatelessWidget {
         ),
         child: Column(
           children: [
+            //Show departure and destination first line with icons
             Row(
               children: [
                 Text(
                   "NYC",
                   style: AppStyles.headLineStyle3.copyWith(color: Colors.white),
                 ),
-                BigDot(),
                 Expanded(child: Container()),
                 BigDot(),
+                Expanded(
+                    child: Stack(
+                  children: [
+                    SizedBox(
+                      height: 24,
+                      child: Center(
+                          child: AppLayoutBuilderWidget(
+                        randomdivider: 6,
+                      )),
+                    ),
+                    Center(
+                      child: Transform.rotate(
+                        angle: pi / 2,
+                        child: Icon(
+                          Icons.local_airport_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+                BigDot(),
+                Expanded(child: Container()),
                 Text(
-                  "NYC",
+                  "LDN",
                   style: AppStyles.headLineStyle3.copyWith(color: Colors.white),
                 ),
               ],
             ),
             Row(
-              children: [],
-            )
+              children: [
+                Text(
+                  "NYC",
+                  style: AppStyles.headLineStyle3.copyWith(color: Colors.white),
+                ),
+                Expanded(child: Container()),
+                BigDot(),
+                Expanded(
+                    child: Stack(
+                  children: [
+                    SizedBox(
+                      height: 24,
+                      child: Center(
+                          child: AppLayoutBuilderWidget(
+                        randomdivider: 6,
+                      )),
+                    ),
+                    Center(
+                      child: Transform.rotate(
+                        angle: pi / 2,
+                        child: Icon(
+                          Icons.local_airport_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+                BigDot(),
+                Expanded(child: Container()),
+                Text(
+                  "LDN",
+                  style: AppStyles.headLineStyle3.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
           ],
         ),
       ),
